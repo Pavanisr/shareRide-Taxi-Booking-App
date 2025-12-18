dca34ba4f1d1e7e9b017bc1fdcbb59661409124a
Pavanisr <pavaniranasinghe425@gmail.com>
2025-12-18 13:19:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,16 @@ const express = require("express");
 const cors = require("cors");
 
 const passengerRoutes = require("./routes/passengerRoutes");
-//const driverRoutes = require("./routes/driverRoutes");
+const driverRoutes = require("./routes/driverRoutes");
 
 const app = express();
-const pool = require("./db"); 
 
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-
 app.use("/api/passenger", passengerRoutes);
-//app.use("/api/driver", driverRoutes);
+app.use("/api/driver", driverRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
